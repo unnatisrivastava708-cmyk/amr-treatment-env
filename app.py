c120949d5892ea110fdfb5922050f5f969710758
unnatisrivastava708-cmyk <unnatisrivastava708@gmail.com>
2026-03-30 21:02:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from env import AMREnv
+
+app = FastAPI()
+env = AMREnv()
+
+class ActionRequest(BaseModel):
+    action: str
+
+@app.post("/reset")
+def reset():
+    state = env.reset()
+    return {"state": state}
+
+@app.post("/step")
+def step(action_req: ActionRequest):
+    state, reward, done = env.step(action_req.action)
+    return {
+        "state": state,
+        "reward": reward,
+        "done": done
+    }
+
+@app.get("/state")
+def get_state():
+    return {"state": env.state}
